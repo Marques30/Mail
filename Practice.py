d5c70ac7381b614009fab5badb704a70cc655d74
Marques30 <marques.butilla@gmail.com>
2016-01-21 09:22:24 -0800

    Logs into yahoo

--- Practice.py
+++ Practice.py
@@ -1,28 +1,41 @@
 from selenium import webdriver
 
-print('Input your google email address')
+print('Input your yahoo email address')
 userEmail = input()
-print('Input your google password')
+print('Input your yahoo password')
 userPassword = input()
+print('Input who you are sending to')
+userSender = input()
+print('Input the subject of email')
+userSubject = input()
 
 browser = webdriver.Firefox()
-browser.get('http://google.com')
+browser.get('http://yahoo.com')
 
-loginElem = browser.find_element_by_link_text('Gmail')
+loginElem = browser.find_element_by_link_text('Mail')
 loginElem.click()
 
-loginElem = browser.find_element_by_link_text('Sign in')
-loginElem.click()
-
-emailElem = browser.find_element_by_id('Email')
+emailElem = browser.find_element_by_id('login-username')
 emailElem.send_keys(userEmail)
 
-loginElem = browser.find_element_by_id('next')
-loginElem.click()
-
-passwordElem = browser.find_element_by_id('Passwd')
+passwordElem = browser.find_element_by_id('login-passwd')
 passwordElem.send_keys(userPassword)
 passwordElem.submit()
 
-loginElem = browser.find_element_by_id('signIn')
+loginElem = browser.find_element_by_id('login-signin')
+loginElem.click()
+
+loginElem = browser.find_element_by_id('skipbtn')
+loginElem.click()
+
+loginElem = browser.find_element_by_partial_link_text('Compose')
+loginElem.click()
+
+emailElem = browser.find_element_by_id('to-field')
+emailElem.send_keys(userSender)
+
+emailElem = browser.find_element_by_id('subject-field')
+emailElem.send_keys(userSubject)
+
+loginElem = browser.find_element_by_link_text('Send')
 loginElem.click()
